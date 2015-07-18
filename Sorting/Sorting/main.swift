b2dbe18833acb9fe1f935a51309e9258ddc3c08a
Ryan Moniz <ryanm@ca.ibm.com>
2015-07-18 11:00:31 -0400

    initial commit of Sorting project

---
+++ Sorting/Sorting/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Sorting
+//
+//  Created by Ryan Moniz on 2015-07-18.
+//  Copyright © 2015 ryanmoniz. All rights reserved.
+//
+// Basic Sorting algorithm in Swift
+
+import Foundation
+
+print("Hello, World!")
+
